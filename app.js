e842c902cb636f9e95e42def6dfb903691daba10
akjmalamin7 <thpi205526@gmail.com>
2022-03-15 16:03:07 +0600

    dependent dropdown

--- app.js
+++ app.js
@@ -1,53 +1,74 @@
 
-let division = "./division.json"
-let district = "./district.json"
-let upazila = "./upazila.json"
-let dist_id;
-let div_id;
+  let division = "./division.json";
+  let district = "./district.json";
+  let upazila = "./upazila.json";
 
-let divisionID = document.getElementById('divisionID')
-let districtID = document.getElementById('districtID')
-let upazilaID = document.getElementById('upazilaID')
-function div(){
-    dist(div_id)
+  let divisionID = document.getElementById("divisionID");
+  let districtID = document.getElementById("districtID");
+  let upazilaID = document.getElementById("upazilaID");
+  let placeDivision = document.getElementById("placeDivision");
+  let form = document.getElementById("form");
+
+  showDivision();
+  function showDivision() {
+    showDistrict();
     fetch(division)
-    .then(res => res.json())
-    .then(data=> Object.entries(data).forEach((div, index)=>{
-        let result;
-        div[1].forEach((item, ind)=>{
-         div_id += item.id
-         result += `<option value=${item.name}>${item.name}</option>`
-        })
-        divisionID.innerHTML = result
-        console.log(div_id)
-    }))
-    
-}
-div()
-function dist(){
-    upa()
-    fetch(district)
-    .then(res => res.json())
-    .then(data=> Object.entries(data).forEach((dist, index)=>{
-        let result;
-        dist[1].forEach((item, ind)=>{
-           
-            result += `<option value=${item.name}>${item.name}</option>`
-        })
-        districtID.innerHTML = result
-    }))
-    
-}
+      .then((res) => res.json())
+      .then((data) =>
+        Object.entries(data).forEach((division) => {
+          let dv;
+          dv += `<option>Select Division</option>`;
+          division[1].forEach((div) => {
+            dv += `<option value=${div.id}>${div.name}</option>`;
+          });
 
-function upa(){
-    fetch(upazila)
-    .then(res => res.json())
-    .then(data=>  Object.entries(data).forEach((upaZila, index)=>{
-        let result;
-        upaZila[1].forEach((item, ind)=>{
-            result += `<option value=${item.name}>${item.name}</option>`
+          divisionID.innerHTML = dv;
         })
-        upazilaID.innerHTML = result
-    }))
-}
-
+      );
+  }
+  function showDistrict() {
+    showUpazila();
+    divisionID.addEventListener("change", function (e) {
+      fetch(district)
+        .then((res) => res.json())
+        .then((data) =>
+          Object.entries(data).forEach((dist, index) => {
+            let district;
+            district += `<option>Select District</option>`;
+            dist[1].forEach((dst) => {
+              if (dst.division_id === e.target.value) {
+                district += `<option value=${dst.id}>${dst.name}</option>`;
+              }
+            });
+            districtID.innerHTML = district;
+          })
+        );
+      placeDivision.innerHTML = e.target.selectedOptions[0].text;
+      upazilaID.innerHTML = `<option>Select Upazila</option>`;
+      placeDistrict.innerHTML = ""
+      placeUpazila.innerHTML = ""
+    });
+  }
+  function showUpazila() {
+    districtID.addEventListener("change", function (e) {
+      fetch(upazila)
+        .then((res) => res.json())
+        .then((data) =>
+          Object.entries(data).forEach((up, index) => {
+            let storeUp;
+            storeUp += `<option>Select Upazila</option>`;
+            up[1].forEach((item) => {
+              if (item.district_id === e.target.value) {
+                storeUp += `<option value=${item.id}>${item.name}</option>`;
+              }
+            });
+            upazilaID.innerHTML = storeUp;
+            placeDistrict.innerHTML = e.target.selectedOptions[0].text;
+            placeUpazila.innerHTML = ""
+          })
+        );
+    });
+    upazilaID.addEventListener("change", function (e) {
+      placeUpazila.innerHTML = e.target.selectedOptions[0].text;
+    });
+  }
